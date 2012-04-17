0ff3bef66f65ed6d4bda5aba8494dbf55d60884a
Benedikt Vormwald <benedikt.vormwald@desy.de>
2012-04-17 17:19:59 +0000

    merging of collection parameters impemented;
    new parameter names (input collection prefix):
    "InputCollectionName_ParameterName"
    
    additional parameters of merged output collection: 
    "MergedCollection_InputCollections":  list of input collections that were merged
    "MergedCollection_NElements":         number of entries in each input collection
    "MergedCollection_NIntParameters":    number of int parameters in each input collection
    "MergedCollection_NFloatParameters":  number of float parameters in each input collection
    "MergedCollection_NStringParameters": number of string parameters in each input collection

--- src/MergeCollections.cc
+++ src/MergeCollections.cc
@@ -1,6 +1,7 @@
 #include "MergeCollections.h"
 
 #include <IMPL/LCCollectionVec.h>
+#include <IMPL/LCParametersImpl.h>
 
 // ----- include for verbosity dependend logging ---------
 #include "marlin/VerbosityLevels.h"
@@ -58,89 +59,117 @@ MergeCollections::MergeCollections() : Processor("MergeCollections") {
 //============================================================================================================================
 
 void MergeCollections::init() { 
-  
   streamlog_out(DEBUG) << "   init called  " << std::endl ;
-  
+
   // usually a good idea to
   printParameters() ;
-  
+
   _nRun = 0 ;
   _nEvt = 0 ;
-  
 }
 //============================================================================================================================
 
 
 void MergeCollections::processRunHeader( LCRunHeader* run) { 
-  
   _nRun++ ;
-} 
+}
 
 
 //============================================================================================================================
 
 void MergeCollections::processEvent( LCEvent * evt ) { 
-  
-  
+  StringVec colNamesPresent;
+  IntVec colNElements;
+  IntVec colNIntParam;
+  IntVec colNFloatParam;
+  IntVec colNStringParam;
+
   unsigned nCol = _inColNames.size() ;
-  
+
   //--- copy existing collections to a vector first
   std::vector<LCCollection*> colVec ;
 
   for( unsigned i=0; i < nCol ; ++i) {
-    
     LCCollection* col  =  getCollection ( evt , _inColNames[i] ) ;
-    
+
     if( col != 0 ){ 
-      
       colVec.push_back( col ) ;
-      
+      colNamesPresent.push_back(_inColNames[i]);
     } else {
-      
       streamlog_out(DEBUG2) << " input collection missing : " << _inColNames[i] << std::endl ;
     }
   }
 
-
   // ---- now loop over existing collections 
   LCCollectionVec* outCol = 0 ;
-
   nCol = colVec.size() ;
 
-  for( unsigned i=0; i < nCol ; ++i) {
-
-    LCCollection* col  =  colVec[i] ;
-
-    if( i == 0 ){
+  for( unsigned k=0; k < nCol ; ++k) {
+    LCCollection* col  =  colVec[k] ;
+    if( k == 0 ){
       // copy collection flags from first collections
-      
       outCol = new LCCollectionVec( col->getTypeName() )  ;
-      
       outCol->setFlag( col->getFlag() ) ;
-      
     }
-      
     int nEle = col->getNumberOfElements() ;
-    
     for(int j=0; j < nEle ; ++j){
-      
       outCol->addElement(  col->getElementAt(j) ) ;
     }
-    
-  }    
-  
+
+    int intParams = 0;
+    int floatParams = 0;
+    int stringParams = 0;
+
+    StringVec intKeys ;
+    int nIntParameters = col->getParameters().getIntKeys( intKeys ).size() ;
+    for(int i=0; i< nIntParameters ; i++ ){
+      IntVec intVec ;
+      col->getParameters().getIntVals(  intKeys[i], intVec ) ;
+      const std::string newIntKey = colNamesPresent[k]+"_"+intKeys[i];
+      outCol->parameters().setValues(newIntKey,intVec);
+      intParams++;
+    }
+
+    StringVec floatKeys ;
+    int nFloatParameters = col->getParameters().getFloatKeys( floatKeys ).size() ;
+    for(int i=0; i< nFloatParameters ; i++ ){
+      FloatVec floatVec ;
+      col->getParameters().getFloatVals(  floatKeys[i], floatVec ) ;
+      const std::string newFloatKey = colNamesPresent[k]+"_"+floatKeys[i];
+      outCol->parameters().setValues(newFloatKey,floatVec);
+      floatParams++;
+    }
+
+    StringVec stringKeys ;
+    int nStringParameters = col->getParameters().getStringKeys( stringKeys ).size() ;
+    for(int i=0; i< nStringParameters ; i++ ){
+      StringVec stringVec ;
+      col->getParameters().getStringVals(  stringKeys[i], stringVec ) ;
+      const std::string newStringKey = colNamesPresent[k]+"_"+stringKeys[i];
+      outCol->parameters().setValues(newStringKey,stringVec);
+      stringParams++;
+    }
+    colNElements.push_back(nEle);
+    colNIntParam.push_back(intParams);
+    colNFloatParam.push_back(floatParams);
+    colNStringParam.push_back(stringParams);
+  }
+
+  outCol->parameters().setValues("MergedCollection_InputCollections",colNamesPresent);
+  outCol->parameters().setValues("MergedCollection_NElements",colNElements);
+  outCol->parameters().setValues("MergedCollection_NIntParameters",colNIntParam);
+  outCol->parameters().setValues("MergedCollection_NFloatParameters",colNFloatParam);
+  outCol->parameters().setValues("MergedCollection_NStringParameters",colNStringParam);
+
   if( outCol ) {
-
-    outCol->setTransient( true ) ;
+    outCol->setTransient( false ) ;
     outCol->setSubset( true ) ;
-
     evt->addCollection( outCol, _outColName   ) ;
   }
 
   streamlog_out(DEBUG) << "   processing event: " << evt->getEventNumber() 
 		       << "   in run:  "          << evt->getRunNumber()   << std::endl ;
-  
-  
+
   _nEvt ++ ;
 }
 
@@ -153,10 +182,8 @@ void MergeCollections::check( LCEvent * evt ) {
 
 //============================================================================================================================
 
-void MergeCollections::end(){ 
-  
+void MergeCollections::end(){
   streamlog_out( MESSAGE ) << " processed " << _nEvt << " events in " << _nRun << " runs "
 			   << std::endl ;
-
 }
 
